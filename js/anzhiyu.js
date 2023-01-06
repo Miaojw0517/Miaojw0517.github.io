c8f5d0a7911f834afaf81806a25b4f4dcb40f9ec
Miaojw0517 <1046665443@qq.com>
2023-01-06 11:24:25 +0800

    Site updated: 2023-01-06 11:23:38

--- js/anzhiyu.js
+++ js/anzhiyu.js
@@ -4,7 +4,6 @@ var anzhiyu_musicFirst = false;
 var anzhiyu = {
   //切换音乐播放状态
   musicToggle: function () {
-     debugger 
     if (!anzhiyu_musicFirst) {
       musicBindEvent();
       anzhiyu_musicFirst = true;
@@ -57,6 +56,103 @@ var anzhiyu = {
     }
     return arr[0];
   },
+  diffDate: function (d, more = false) {
+    const dateNow = new Date();
+    const datePost = new Date(d);
+    const dateDiff = dateNow.getTime() - datePost.getTime();
+    const minute = 1000 * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const month = day * 30;
+
+    let result;
+    if (more) {
+      const monthCount = dateDiff / month;
+      const dayCount = dateDiff / day;
+      const hourCount = dateDiff / hour;
+      const minuteCount = dateDiff / minute;
+
+      if (monthCount >= 1) {
+        result = datePost.toLocaleDateString().replace(/\//g, "-");
+      } else if (dayCount >= 1) {
+        result = parseInt(dayCount) + " " + GLOBAL_CONFIG.date_suffix.day;
+      } else if (hourCount >= 1) {
+        result = parseInt(hourCount) + " " + GLOBAL_CONFIG.date_suffix.hour;
+      } else if (minuteCount >= 1) {
+        result = parseInt(minuteCount) + " " + GLOBAL_CONFIG.date_suffix.min;
+      } else {
+        result = GLOBAL_CONFIG.date_suffix.just;
+      }
+    } else {
+      result = parseInt(dateDiff / day);
+    }
+    return result;
+  },
+  changeTimeInEssay: function () {
+    document.querySelector("#bber") &&
+      document.querySelectorAll("#bber time").forEach(function (e) {
+        var t = e,
+          datetime = t.getAttribute("datetime");
+        (t.innerText = anzhiyu.diffDate(datetime, true)), (t.style.display = "inline");
+      });
+  },
+  reflashEssayWaterFall: function () {
+    document.querySelector("#waterfall") &&
+      setTimeout(function () {
+        waterfall("#waterfall");
+        document.getElementById("waterfall").classList.add("show");
+      }, 500);
+  },
+  commentText: function (txt) {
+    const postCommentDom = document.querySelector("#post-comment");
+    var domTop = postCommentDom.offsetTop;
+    window.scrollTo(0, domTop - 80);
+    if (txt == "undefined" || txt == "null") txt = "好棒！";
+    function setText() {
+      setTimeout(() => {
+        var input = document.getElementsByClassName("el-textarea__inner")[0];
+        if (!input) setText();
+        let evt = document.createEvent("HTMLEvents");
+        evt.initEvent("input", true, true);
+        let inputValue = replaceAll(txt, "\n", "\n> ");
+        input.value = "> " + inputValue + "\n\n";
+        input.dispatchEvent(evt);
+        input.focus();
+        input.setSelectionRange(-1, -1);
+        if (document.getElementById("comment-tips")) {
+          document.getElementById("comment-tips").classList.add("show");
+        }
+      }, 100);
+    }
+    setText();
+  },
+  initIndexEssay: function () {
+    setTimeout(() => {
+      let essay_bar_swiper = new Swiper(".essay_bar_swiper_container", {
+        passiveListeners: true,
+        direction: "vertical",
+        loop: true,
+        autoplay: {
+          disableOnInteraction: true,
+          delay: 3000,
+        },
+        mousewheel: true,
+      });
+
+      let essay_bar_comtainer = document.getElementById("bbtalk");
+      if (essay_bar_comtainer !== null) {
+        essay_bar_comtainer.onmouseenter = function () {
+          essay_bar_swiper.autoplay.stop();
+        };
+        essay_bar_comtainer.onmouseleave = function () {
+          essay_bar_swiper.autoplay.start();
+        };
+      }
+    }, 100);
+  },
 };
 
 // addRightMenuClickEvent();
+anzhiyu.initIndexEssay();
+anzhiyu.changeTimeInEssay();
+anzhiyu.reflashEssayWaterFall();
